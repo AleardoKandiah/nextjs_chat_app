f93fdb150675b593c9e8c1c9372e0caa855a12f2
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-05 18:57:47 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import React, { useState, createContext} from 'react';
 
+// Initialises instance of context API 
+export const Context = createContext();
+
+export const ContextProvider = () = {
+  const [username, setUsername] = useState('');
+  const [secret, setSecret] = useState('');
+}
 export default function Auth() {
   return <div className='background'>auth</div>;
 }
+
