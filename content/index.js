836cad593f440bbc09556bf08108c2402567b200
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-05 19:20:00 +0100

    initial commit

---
+++ content/index.js
@@ -0,0 +1,22 @@
+import React, { useState, createContext} from 'react';
+
+// First component Initialises instance of context API 
+export const Context = createContext();
+
+// Second component is a provider to hold other components for context access
+export const ContextProvider = (props) => {
+  const [username, setUsername] = useState('');
+  const [secret, setSecret] = useState('');
+
+  // Shared const are username and secret to recognise user presence
+  // This will be used to understand which page to render
+  const value = {
+  username,
+  setUsername,
+  secret,
+  setSecret,
+  };
+
+  // These must be passed into a value prop in the provider
+  return <Context.Provider value={value}>{props.children}</Context.Provider>;
+};
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import '../styles/auth.css';
 import '../styles/index.css';
 import '../styles/chats.css';
 
-// import { ContextProvider} from '../content'
+import { ContextProvider } from '../content';
 
-
+// Allow access to user details regardless of page by wrapping the entire application in the context provider
 export default function App({ Component, pageProps }) {
     return (
-      // Context provider
-      <Component {...pageProps}/>
+      <ContextProvider>
+        <Component {...pageProps}/>
+      </ContextProvider>
       
-     /* Context provider   */
-    )
-};
+    );
+}
