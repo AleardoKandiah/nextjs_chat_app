2ff13a3638faed653ce1384e04953e074c7713a6
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-05 18:59:45 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import React, { useState, createContext} from 'react';
 
-// Initialises instance of context API 
+// First component Initialises instance of context API 
 export const Context = createContext();
 
-export const ContextProvider = () = {
+// Second component is a provider to hold other components for context access
+export const ContextProvider = () => {
   const [username, setUsername] = useState('');
   const [secret, setSecret] = useState('');
+
+  const value = {
+  username,
+  setUsername,
+  secret,
+  setSecret,
+  };
+
+  return <Context.Provider value={value}>{props.children}</Context.Provider>;
 }
 export default function Auth() {
   return <div className='background'>auth</div>;
