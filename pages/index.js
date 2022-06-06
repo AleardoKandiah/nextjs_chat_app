f86503917fd5fa745732941de709330ad0b54e15
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-06 21:08:11 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-import React from 'react';
+// import usecontext to access context state data from index.js
+import React, {useContext} from 'react';
+import { Context } from '../content';
+
+// this is imported so that upon login we are redirected to chat page
+import {useRouter} from 'next/router';
+
+// Downloaded axios client from yarn is then imported
+import axios from 'axios';
 
 export default function Auth() {
   return <div className='background'>auth</div>;
