f00f9eb473eee0020717d9b91786997bba6bf25f
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-06-07 21:40:35 +0100

    initial commit

--- content/index.js
+++ content/index.js
@@ -8,13 +8,17 @@ export const ContextProvider = (props) => {
   const [username, setUsername] = useState('');
   const [secret, setSecret] = useState('');
 
+// when value assigned to setUsername, the value can be later picked up by username
+
+
   // Shared const are username and secret to recognise user presence
   // This will be used to understand which page to render
+
   const value = {
   username,
   setUsername,
   secret,
-  setSecret,
+  setSecret
   };
 
   // These must be passed into a value prop in the provider
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,28 @@ import {useRouter} from 'next/router';
 import axios from 'axios';
 
 export default function Auth() {
+  // Import Context
+  const { username, setUsername, secret, setSecret } = useContext(Context) 
+
   // Build JSX
   return (
   <div className='background'>
     <div className='auth-container'>
       {/* create a form with function */}
       <form className='auth-form' onSubmit={e => e.preventDefault()}>
+        <div className='auth-title'>Sky Chat</div>
 
+        {/* ADD INPUTS */}
+        <div className='input-container'> 
+          {/* Add props in the input */}
+          <input
+            placeholder='email' 
+            className='text-input'  
+            onChange={ e => setUsername(e.target.value)}
+            // Onchange will be where the context API will be used by triggering the e event       
+          />
+
+        </div>
       </form>
     </div>
   </div>
